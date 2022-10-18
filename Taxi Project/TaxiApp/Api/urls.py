5e5d18acc1cbe245147b43aa0516e54eaad72e7e
hashem-muhammad <53443536+hashem-muhammad@users.noreply.github.com>
2022-10-18 20:52:32 +0300

    fix urls.py

--- Taxi Project/TaxiApp/Api/urls.py
+++ Taxi Project/TaxiApp/Api/urls.py
@@ -1,26 +1,8 @@
-from django.urls import path, include, re_path
+from django.urls import path, include
 
 from Api.views import CarTypeView, DriverCarView, MyObtainTokenPairView, MessageView, StopPointView, TripTypeView, TripView
 
-
-from rest_framework import permissions
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
-
-schema_view = get_schema_view(
-    openapi.Info(
-        title="TAXI APP API",
-        default_version='v1',
-        description="API description",
-    ),
-    public=True,
-    permission_classes=(permissions.AllowAny,),
-)
-
-
 urlpatterns = [
-    re_path(r'^docs/$', schema_view.with_ui('redoc',
-                                            cache_timeout=0), name='schema-redoc'),
     path('auth/register/', include('dj_rest_auth.registration.urls')),
     path('auth/', include('dj_rest_auth.urls')),
     path('login/', MyObtainTokenPairView.as_view(), name="token_obtain_pair"),

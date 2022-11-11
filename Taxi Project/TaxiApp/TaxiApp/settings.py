348d4f0bf15b92ad935e9282033544c45d16f947
hashem-muhammad <53443536+hashem-muhammad@users.noreply.github.com>
2022-11-11 15:56:19 +0200

    V8.2

--- Taxi Project/TaxiApp/TaxiApp/settings.py
+++ Taxi Project/TaxiApp/TaxiApp/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = 'django-insecure-n3hw*_pl58eib5*ke&_mdrxg+w9t0mch2j&&0a9$k-p5^u6lhc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['95.159.59.7']
 
 
 # Application definition

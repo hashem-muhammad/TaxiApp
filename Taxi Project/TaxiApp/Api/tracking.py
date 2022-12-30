79b9352b07be76fe431383fce4b9ae0c489e1bab
hashem-muhammad <53443536+hashem-muhammad@users.noreply.github.com>
2022-12-30 18:03:02 +0400

    V15

--- Taxi Project/TaxiApp/Api/tracking.py
+++ Taxi Project/TaxiApp/Api/tracking.py
@@ -119,29 +119,71 @@ class DriverCounsumer(AsyncWebsocketConsumer):
     async def receive(self, text_data=None):
         driver = self.scope['url_route']['kwargs']['room_name']
         text_data_json = json.loads(text_data)
-        lat = text_data_json['lat']
-        lng = text_data_json['lng']
+        start_place = text_data_json['start_place']
+        source = text_data_json['source']
+        destination = text_data_json['destination']
+        final_place = text_data_json['final_place']
+        user = text_data_json['user']
+        time_ending = text_data_json['time_ending']
+        expected_time = text_data_json['expected_time']
+        distance = text_data_json['distance']
+        price = text_data_json['price']
         status = text_data_json['status']
+        trip_type = text_data_json['trip_type']
+        price_after_coupon = text_data_json['price_after_coupon']
+        trip_cancellation = text_data_json['trip_cancellation']
         await self.channel_layer.group_send(
             self.room_group_name,
             {
                 'type': 'tracking',
                 'driver_id': driver,
-                'lat': lat,
-                'lng': lng,
-                'status': status
+                'start_place': start_place,
+                'source': source,
+                'status': status,
+                'destination':destination,
+                'final_place':final_place,
+                'user':user,
+                'time_ending':time_ending,
+                'expected_time':expected_time,
+                'distance':distance,
+                'price':price,
+                'trip_type':trip_type,
+                'price_after_coupon':price_after_coupon,
+                'trip_cancellation':trip_cancellation
             }
         )
 
     async def tracking(self, event):
-        user = event['driver_id']
-        lat = event['lat']
-        lng = event['lng']
+        driver = event['driver_id']
+        start_place = event['start_place']
+        source = event['source']
         status = event['status']
 
+        destination = event['destination']
+        final_place = event['final_place']
+        user = event['user']
+
+        time_ending = event['time_ending']
+        distance = event['distance']
+        price = event['price']
+        trip_type = event['trip_type']
+        price_after_coupon = event['price_after_coupon']
+        trip_cancellation = event['trip_cancellation']
+
         await self.send(text_data=json.dumps({
-            'driver_id': user,
-            'lat': lat,
-            'lng': lng,
-            'status':status
+            'driver_id': driver,
+            'start_place': start_place,
+            'source': source,
+            'status':status,
+
+            'destination': destination,
+            'final_place': final_place,
+            'user':user,
+
+            'time_ending': time_ending,
+            'distance': distance,
+            'price':price,
+            'trip_type': trip_type,
+            'price_after_coupon':price_after_coupon,
+            'trip_cancellation':trip_cancellation
         }))

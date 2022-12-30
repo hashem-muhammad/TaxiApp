8ccef5cf2d8e6ba010bc4c22942eae35fc71a499
hashem-muhammad <53443536+hashem-muhammad@users.noreply.github.com>
2022-12-30 17:42:30 +0400

    V15

--- Taxi Project/TaxiApp/TaxiApp/routing.py
+++ Taxi Project/TaxiApp/TaxiApp/routing.py
@@ -1,7 +1,8 @@
 from django.urls import re_path
-from Api.tracking import TrackingCounsumer, TrackingDriverCounsumer
+from Api.tracking import DriverCounsumer, TrackingCounsumer, TrackingDriverCounsumer
 
 websocket_url = [
     re_path(r'ws/tracking/(?P<room_name>\w+)/$', TrackingCounsumer.as_asgi()),
     re_path(r'ws/tracking/driver/(?P<room_name>\w+)/$', TrackingDriverCounsumer.as_asgi()),
+    re_path(r'ws/driver/(?P<room_name>\w+)/$', DriverCounsumer.as_asgi()),
 ]
